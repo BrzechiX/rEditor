f37772c02663eb45877fdb5e18fabe9d4fde5c4f
BrzechiX <brzechix@gmail.com>
2024-05-31 02:57:52 +0200

    added something

---
+++ service-worker.js
@@ -0,0 +1,60 @@
+const CACHE_NAME = 'reditor-cache-31-05-2024-v10';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/favicon.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.65.5/codemirror.min.css',
+  'https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.65.5/theme/dracula.min.css',
+  'https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.65.5/codemirror.min.js',
+  '/syntax.js',
+  '/profiles/potados.jsonc',
+  '/profiles/cpu5.jsonc',
+  '/profiles/pm1.jsonc'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request).then(
+          response => {
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+            let responseToCache = response.clone();
+            caches.open(CACHE_NAME)
+              .then(cache => {
+                cache.put(event.request, responseToCache);
+              });
+            return response;
+          }
+        );
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(keyList.map(key => {
+        if (cacheWhitelist.indexOf(key) === -1) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+});
--- syntax.js
+++ syntax.js
@@ -1,52 +1,206 @@
-CodeMirror.defineMode("mineasm", function() {
-    return {
-        startState: function() {
-            return {
-                inString: false
-            };
-        },
-        token: function(stream, state) {
-            if (stream.eatSpace()) return null;
-
-            if (stream.match("//")) {
-                stream.skipToEnd();
-                return "comment";
+function fetchProfileCommands(profileName) {
+    return new Promise((resolve, reject) => {
+        fetch(`./profiles/${profileName}.jsonc`)
+        .then(response => response.text())
+        .then(text => {
+            let commandsArray = [];
+            text = JSON.parse(text);
+            for (let command in text.CPU.COMMANDS) {
+                if (text.CPU.COMMANDS.hasOwnProperty(command)) {
+                    let pattern = text.CPU.COMMANDS[command].pattern;
+                    let firstWord = pattern.split(' ')[0];
+                    commandsArray.push(firstWord);
+                }
             }
+            commandsArray.sort((a, b) => b.localeCompare(a));
+            resolve(commandsArray);
+        })
+        .catch(error => {
+            console.error('Error: ', error);
+            reject(error);
+        });
+    });
+}
 
-            if (!state.inString && stream.match('"')) {
-                state.inString = true;
-            }
-            if (state.inString) {
-                if (stream.skipTo('"')) {
-                    stream.next();
-                    state.inString = false;
-                } else {
+(async function() {
+    let potadosCommands = await fetchProfileCommands('potados');
+    CodeMirror.defineMode("potados", function() {
+        return {
+            startState: function() {
+                return {
+                    inString: false
+                };
+            },
+            token: function(stream, state) {
+                if (stream.eatSpace()) return null;
+
+                if (stream.match("//")) {
                     stream.skipToEnd();
+                    return "comment";
                 }
-                return "string";
-            }
 
-            if (stream.match(/^\d+/)) {
-                return "number";
-            }
-
-            if (stream.match(/^#[a-zA-Z_]\w*/) || stream.match(/^\.[a-zA-Z_]\w*/)) {
-                return "metaa";
-            }
-
-            if (stream.match(/^[a-zA-Z_]\w*:/)) {
-                return "label";
-            }
-
-            var keywords = ["cmp", "jz", "push", "pop", "add", "rsh", "mov", "dec", "inc", "sub", "jne", "call", "ret", "int", "jge", "jle", "jl", "jg", "jmp", "je", "jne", ","];
-            for (var i = 0; i < keywords.length; i++) {
-                if (stream.match(keywords[i])) {
-                    return "keyword";
+                if (!state.inString && stream.match('"')) {
+                    state.inString = true;
                 }
-            }
+                if (state.inString) {
+                    if (stream.skipTo('"')) {
+                        stream.next();
+                        state.inString = false;
+                    } else {
+                        stream.skipToEnd();
+                    }
+                    return "string";
+                }
 
-            stream.next();
-            return null;
-        }
-    };
-});
+                if (stream.match(/^\d+/)) {
+                    return "number";
+                }
+
+                if (stream.match(/^#[a-zA-Z_]\w*/)) {
+                    return "preprocessor";
+                }
+
+                if (stream.match(/^\.[a-zA-Z_]\w*/)) {
+                    return "wymysllorda";
+                }
+
+                if (stream.match(/^[a-zA-Z_]\w*:/)) {
+                    return "label";
+                }
+
+                if (stream.match(',')) {
+                    return "comma";
+                }
+
+                for (var i = 0; i < potadosCommands.length; i++) {
+                    if (stream.match(potadosCommands[i])) {
+                        return "keyword";
+                    }
+                }
+
+                stream.next();
+                return null;
+            }
+        };
+    });
+    let cpu5Commands = await fetchProfileCommands('cpu5');
+    CodeMirror.defineMode("cpu5", function() {
+        return {
+            startState: function() {
+                return {
+                    inString: false
+                };
+            },
+            token: function(stream, state) {
+                if (stream.eatSpace()) return null;
+
+                if (stream.match("//")) {
+                    stream.skipToEnd();
+                    return "comment";
+                }
+
+                if (!state.inString && stream.match('"')) {
+                    state.inString = true;
+                }
+                if (state.inString) {
+                    if (stream.skipTo('"')) {
+                        stream.next();
+                        state.inString = false;
+                    } else {
+                        stream.skipToEnd();
+                    }
+                    return "string";
+                }
+
+                if (stream.match(/^\d+/)) {
+                    return "number";
+                }
+
+                if (stream.match(/^#[a-zA-Z_]\w*/)) {
+                    return "preprocessor";
+                }
+
+                if (stream.match(/^\.[a-zA-Z_]\w*/)) {
+                    return "wymysllorda";
+                }
+
+                if (stream.match(/^[a-zA-Z_]\w*:/)) {
+                    return "label";
+                }
+
+                if (stream.match(',')) {
+                    return "comma";
+                }
+
+                for (var i = 0; i < cpu5Commands.length; i++) {
+                    if (stream.match(cpu5Commands[i])) {
+                        return "keyword";
+                    }
+                }
+
+                stream.next();
+                return null;
+            }
+        };
+    });
+    let pm1Commands = await fetchProfileCommands('pm1');
+    CodeMirror.defineMode("pm1", function() {
+        return {
+            startState: function() {
+                return {
+                    inString: false
+                };
+            },
+            token: function(stream, state) {
+                if (stream.eatSpace()) return null;
+
+                if (stream.match("//")) {
+                    stream.skipToEnd();
+                    return "comment";
+                }
+
+                if (!state.inString && stream.match('"')) {
+                    state.inString = true;
+                }
+                if (state.inString) {
+                    if (stream.skipTo('"')) {
+                        stream.next();
+                        state.inString = false;
+                    } else {
+                        stream.skipToEnd();
+                    }
+                    return "string";
+                }
+
+                if (stream.match(/^\d+/)) {
+                    return "number";
+                }
+
+                if (stream.match(/^#[a-zA-Z_]\w*/)) {
+                    return "preprocessor";
+                }
+
+                if (stream.match(/^\.[a-zA-Z_]\w*/)) {
+                    return "wymysllorda";
+                }
+
+                if (stream.match(/^[a-zA-Z_]\w*:/)) {
+                    return "label";
+                }
+
+                if (stream.match(',')) {
+                    return "comma";
+                }
+
+                for (var i = 0; i < pm1Commands.length; i++) {
+                    if (stream.match(pm1Commands[i])) {
+                        return "keyword";
+                    }
+                }
+
+                stream.next();
+                return null;
+            }
+        };
+    });
+})();
